cbfce8e82d1df03c0d7de204702a50c32e354b99
COL073 <nicolecolleta9@gmail.com>
2024-03-02 13:02:03 +0300

    first commit

---
+++ dart datatypes.dart
@@ -0,0 +1,37 @@
+void main() {
+  // declare an integer (whole number)
+  int num1 = 5;
+  //declare a double value(decimal point)
+  double num2 = 2.2;
+  //print the values
+  print(num1);
+  print(num2);
+
+  // for sum
+  num sum = num1 + num2;
+
+  //print the values
+  print('sum is $sum');
+   
+String name = 'Sandwich White';
+String type = 'Bread';
+//print values
+print("The delicious $name is an amesome $type");
+
+ bool isdelicious= false;
+print("Rating: $isdelicious");
+
+//list containing ordered objects
+List<String> names= ['Elephant', 'COckroach', 'Atlantis', 'cake','Jane2'];
+//printing values
+print("$names");
+print("names[0] is ${names[0]}");// index 0
+
+// Creating a map 
+Map< String, int > weight = {'Tabitha': 45, 'Peter' : 30, 'teacher': 76,};
+print("$weight");
+
+}
+
+
+
